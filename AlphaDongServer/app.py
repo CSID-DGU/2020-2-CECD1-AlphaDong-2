4cb823e870bc018b0b22fa1d1c5e4ac72984f610
yongsk006 <yongsk0066@naver.com>
2020-10-30 07:13:36 +0900

    docker setting

--- AlphaDongServer/app.py
+++ AlphaDongServer/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({'method':request.method})
+
+@app.route("/detect", methods=['POST'])
+def detect():
+    if request.method == 'POST':
+        # file = request.files['file']
+        # img_bytes = file.read()
+        return jsonify({'method':request.method})
+    else:
+        return jsonify({'method':request.method})
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0')

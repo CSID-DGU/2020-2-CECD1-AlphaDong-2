01202e861cd69fda77d6a796b65a8eabc5b1ed97
yongsk006 <yongsk0066@naver.com>
2020-10-30 02:49:11 +0900

    backend docker setting

--- AlphaDongServer/app.py
+++ AlphaDongServer/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0')

85555835927daccf8c6b13c36e8e859a75c96737
yongsk0066 <yongsk0066@naver.com>
2021-02-02 22:06:58 +0900

    [Server] add bash file

---
+++ AlphaDongServer/init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt install apt-transport-https
+
+sudo apt install ca-certificates
+
+sudo apt install curl
+
+sudo apt install software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt install docker-ce

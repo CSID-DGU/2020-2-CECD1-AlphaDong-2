2611a54d18115f9630d61b1cf087b129cb231480
yongsk0066 <yongsk0066@naver.com>
2021-02-11 22:01:09 +0900

    shell update

--- AlphaDongServer/docker.sh
+++ AlphaDongServer/docker.sh
@@ -7,7 +7,7 @@ sudo docker build . -t python:flaskapp
 if ["d" == "$1"]; then
     sudo docker run -d --name flaskhello -p 5001:5001 python:flaskapp
 else
-    sudo docker run --name flaskhello -p 5001:5001 python:flaskapp
+    sudo docker run -it --name flaskhello -p 5001:5001 python:flaskapp
 fi
 
 sudo docker ps -a

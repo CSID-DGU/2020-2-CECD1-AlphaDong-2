71c716a45a6fa272de500840f7f0e0e0e6576aba
yongsk0066 <yongsk0066@naver.com>
2021-03-22 11:58:47 +0900

    server setting

--- AlphaDongServer/detect.py
+++ AlphaDongServer/detect.py
@@ -5,34 +5,34 @@ import random
 def vin_detect(image):
     # time.sleep(random.randrange(2, 5))
     return "1G1YZ23J9P5803427"
-import base64
-from PIL import Image
-from io import BytesIO
-import os
-import json
-from PIL import Image
+# import base64
+# from PIL import Image
+# from io import BytesIO
+# import os
+# import json
+# from PIL import Image
 
-folder = "./GT/input_images/"
+# folder = "./GT/input_images/"
 
 
-def makeRequest(f_names):
+# def makeRequest(f_names):
 
-    req = {"no_img": len(f_names[:]), "image": [], "size_img": [], "filename": []}
-    for f_name in f_names[:]:
+#     req = {"no_img": len(f_names[:]), "image": [], "size_img": [], "filename": []}
+#     for f_name in f_names[:]:
 
-        path = folder + f_name
-        file = open(path, "rb")
-        encoded = base64.b64encode(file.read())
-        base64_string = encoded.decode("utf-8")
-        image_string = BytesIO(base64.b64decode(base64_string))
-        width, height = Image.open(image_string).size
+#         path = folder + f_name
+#         file = open(path, "rb")
+#         encoded = base64.b64encode(file.read())
+#         base64_string = encoded.decode("utf-8")
+#         image_string = BytesIO(base64.b64decode(base64_string))
+#         width, height = Image.open(image_string).size
 
-        req["image"].append(base64_string)
-        req["size_img"].append([width, height])
-        req["filename"].append(f_name)
+#         req["image"].append(base64_string)
+#         req["size_img"].append([width, height])
+#         req["filename"].append(f_name)
 
-    print(len(req["image"]))
-    ## make request file
-    f = open("inputfile_seg_2img.json", "w")
-    f.write(json.dumps(req))
-    f.close()
+#     print(len(req["image"]))
+#     ## make request file
+#     f = open("inputfile_seg_2img.json", "w")
+#     f.write(json.dumps(req))
+#     f.close()
